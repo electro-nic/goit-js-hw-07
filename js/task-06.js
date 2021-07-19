c40ad37a67dccf1e559f9dc67576c9ee172e13db
YEVHEN BATSMAN <evgenbatsman@gmail.com>
2021-07-19 19:38:29 +0300

    update task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,11 @@
-const inputEl = document.querySelector("#validation-input");
-
-
+const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('change', (event) => {
     if (Number(inputEl.dataset.length) !== inputEl.value.length) {
         inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
     }
     else {
-        inputEl.classList.replace('invalid', 'valid');
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
     }
 });
